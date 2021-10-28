1387e3e5f88de3a6ce1d8dccfe2f169915143d86
Balthier Lionheart <raptors.ftw@gmail.com>
2021-10-28 18:04:38 +0100

    Reworked the TVDB plugin

--- plugins/tvdb.js
+++ plugins/tvdb.js
@@ -1,51 +1,11 @@
 tvdb = {
     metadata_xml: null,
 
-    init: function (metadata_xml, type) {
-        tvdb.metadata_xml = metadata_xml;
-
-        tvdb.getTvdbId();
+    init: function (metadata_xml) {
+        tvdb.insertTvdbLink(metadata_xml);
     },
 
-    getTvdbId: async () => {
-        utils.debug("tvdb plugin: Grabbing tvdb id");
-        var agent = tvdb.metadata_xml.getElementsByTagName("MediaContainer")[0].getElementsByTagName("Directory")[0].getAttribute("guid");
-
-        // check if using the tvdb metadata agent
-        if (/com\.plexapp\.agents\.thetvdb/.test(agent)) {
-            var tvdb_id = agent.match(/^com\.plexapp\.agents\.thetvdb:\/\/(\d+)\?/)[1];
-            utils.debug("tvdb plugin: tvdb id found - " + tvdb_id);
-        }
-        else {
-            tv_year = tvdb.metadata_xml.getElementsByTagName("MediaContainer")[0].getElementsByTagName("Directory")[0].getAttribute("year");
-            tv_title = tvdb.metadata_xml.getElementsByTagName("MediaContainer")[0].getElementsByTagName("Directory")[0].getAttribute("title");
-            utils.debug("tvdb plugin: Not using thetvdb agent, attempting search via TMDB using title (" + tv_title + ") and year (" + tv_year + ")");
-            var api_url = "https://api.themoviedb.org/3/search/tv?language=en-US&page=1&include_adult=false&query=" + tv_title + "&first_air_date_year=" + tv_year + "&api_key=" + tmdb_api.api_key;
-            utils.debug("tvdb plugin: Connecting to endpoint" + api_url);
-            response = await fetch(api_url);
-            json = await response.json();
-            var tmdb_id = await json.results[0].id;
-            if (tmdb_id) {
-                utils.debug("tvdb plugin:  tmdb id found - " + tmdb_id);
-                var api_url = "https://api.themoviedb.org/3/tv/" + tmdb_id + "/external_ids?api_key=" + tmdb_api.api_key;
-                utils.debug("tvdb plugin: Connecting to endpoint" + api_url);
-                response = await fetch(api_url);
-                json = await response.json();
-                var tvdb_id = json.tvdb_id;
-            }
-        }
-        if (tvdb_id) {
-            utils.debug("tvdb plugin: tvdb id found - " + tvdb_id);
-            var tvdb_link = tvdb.createTvdbLink(tvdb_id);
-            tvdb.insertTvdbLink(tvdb_link);
-        }
-        else {
-            utils.debug("tvdb plugin: tvdb id not found. Aborting.");
-            return;
-        }
-    },
-
-    createTvdbLink: function (tvdb_id) {
+    constructTvdbLink: function (tvdb_id) {
         var logo_url = utils.getResourcePath("tvdb/tvdb_logo.png");
         var sister_containers = document.querySelectorAll("[class*=sprinkles_display_flex]")[2].children;
         var container_element_template = sister_containers[0];
@@ -73,9 +33,20 @@ tvdb = {
         return tvdb_container_element;
     },
 
-    insertTvdbLink: function (tvdb_link) {
+    insertTvdbLink: async (metadata_xml) => {
         // insert tvdb link element to bottom of metadata container
-        utils.debug("tvdb plugin: Inserting tvdb container into page");
-        document.querySelectorAll("[class*=sprinkles_display_flex]")[2].appendChild(tvdb_link);
+        var type = "show";
+        var site = "tvdb"
+        utils.debug("TVDB Plugin: Lauching TMDB API (Site: " + site + ") (Type: " + type + ")");
+        var tvdb_id = await tmdb_api.getId(site, type, metadata_xml);
+        if (tvdb_id) {
+            utils.debug("TVDB Plugin: TMDB API returned the following TVDB ID (" + tvdb_id + ")");
+            var tvdb_link = tvdb.constructTvdbLink(tvdb_id);
+            utils.debug("TVDB Plugin: Inserting TVDB container into page");
+            document.querySelectorAll("[class*=sprinkles_display_flex]")[2].appendChild(tvdb_link);
+        }
+        else {
+            utils.debug("TVDB Plugin: TMDB API did not find the TVDB ID... Aborting.");
+        }
     }
 }
