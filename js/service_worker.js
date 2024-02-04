d26def82885ad051ed878a50fe38367115ceddf1
Balthier Lionheart <9468615+Balthier@users.noreply.github.com>
2024-02-04 19:18:52 +0000

    Show changelog on update

--- js/service_worker.js
+++ js/service_worker.js
@@ -1,11 +1,15 @@
 chrome.runtime.onInstalled.addListener(function (details) {
     if (details.reason === "install") {
         chrome.tabs.create({ url: "/resources/extras/options.html" });
-    } else if (details.reason === "update") {
+    }
+    else if (details.reason === "update") {
         // When extension is updated
-    } else if (details.reason === "chrome_update") {
+        chrome.tabs.create({ url: "/resources/extras/changelog.html" });
+    }
+    else if (details.reason === "chrome_update") {
         // When browser is updated
-    } else if (details.reason === "shared_module_update") {
+    }
+    else if (details.reason === "shared_module_update") {
         // When a shared module is updated
     }
 });
